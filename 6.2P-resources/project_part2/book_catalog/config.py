b5d6067168531a8d77c460e0a532a2600ce51885
Hashi1995 <s223495239@deakin.edu.au>
2024-09-12 21:17:39 +1000

    second commit

--- 6.2P-resources/project_part2/book_catalog/config.py
+++ 6.2P-resources/project_part2/book_catalog/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://book_catalog_xheb_user:4ZSG0aGSFwZw9ZmNQvMPQxkb10iztHGD@dpg-crhb3ng8fa8c738v3pvg-a.singapore-postgres.render.com/book_catalog_xheb")
 
 settings = Settings()
